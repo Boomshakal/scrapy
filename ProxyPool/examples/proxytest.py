d92bf8c21330e1ddf077a77fa23cc02822cea20d
Li huimin <362169885@qq.com>
2019-04-09 11:30:57 +0800

    push ProxyPool

---
+++ ProxyPool/examples/proxytest.py
@@ -0,0 +1,27 @@
+import requests
+from proxypool.setting import TEST_URL
+from bs4 import BeautifulSoup
+
+def get_proxy():
+    r = requests.get('http://127.0.0.1:5555/random')
+    proxy = BeautifulSoup(r.text, "lxml").get_text()
+    return proxy
+
+
+def crawl(url, proxy):
+    proxies = {'http': proxy}
+    r = requests.get(url, proxies=proxies)
+    return r.text
+
+proxy = get_proxy()
+
+proxies = {
+    'http': 'http://' + proxy,
+    'https': 'https://' + proxy,
+}
+
+print(TEST_URL)
+response = requests.get(TEST_URL, proxies=proxies, verify=False)
+if response.status_code == 200:
+    print('Successfully')
+    print(response.text)
